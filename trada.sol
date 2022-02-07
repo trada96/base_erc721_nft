1aa4301bbb14689f2ba012384c793562191106fb
Luong Van Tuan Anh <luongvantuananh@gapo.com.vn>
2022-02-07 14:40:00 +0700

    add base721

--- trada.sol
+++ trada.sol
@@ -1,16 +1,18 @@
 pragma solidity ^0.8.0;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/ERC721.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 
 contract TradaNFT is ERC721 {
     using Strings for uint256;
     string public baseURI = "https://raw.githubusercontent.com/trada96/base_erc721_nft/master/images/";
     
-    constructor() ERC71("Trada NFT", "ICETEANFT"){}
+    constructor() ERC721("Trada NFT", "ICETEANFT"){
+    }
+    
 
-     function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
         require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
 
         string memory baseURI = _baseURI();
