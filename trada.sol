de2c43dcd2bd12c8b531700f1bf18b709ccaba31
Luong Van Tuan Anh <luongvantuananh@gapo.com.vn>
2022-02-08 14:00:40 +0700

    update license

--- trada.sol
+++ trada.sol
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
@@ -11,13 +13,14 @@ contract TradaNFT is ERC721 {
     constructor() ERC721("Trada NFT", "ICETEANFT"){
     }
     
-
+    
     function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
         require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
 
-        string memory baseURI = _baseURI();
-        return bytes(baseURI).length > 0 ? string(abi.encodePacked(baseURI, tokenId.toString(), ".json"))
-        : "";
+        string memory buri = baseURI;
+        return bytes(buri).length > 0
+            ? string(abi.encodePacked(buri, tokenId.toString(), ".json"))
+            : '';
     }
 
     function mint(address to, uint256 tokenId) external {
